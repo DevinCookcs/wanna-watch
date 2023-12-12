ec46550b5ff96adb673821e49b937db6ad1b684a
Devin Cook <144957285+DevinCookcs@users.noreply.github.com>
2023-12-12 15:41:57 -0700

    removed previous star rating testing

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
 import { useState } from "react";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "OK", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
-
-    <Test />
+    <App />
   </React.StrictMode>
 );
