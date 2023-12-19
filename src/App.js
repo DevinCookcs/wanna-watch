3105e893897c5dc417d68fc538a7031634d35757
Devin Cook <144957285+DevinCookcs@users.noreply.github.com>
2023-12-19 14:30:32 -0700

    added useEffect to listen for escape key to close out of currently selected movie

--- src/App.js
+++ src/App.js
@@ -30,6 +30,14 @@ export default function App() {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
 
+  useEffect(function () {
+    document.addEventListener("keydown", function (e) {
+      if (e.code === "Escape") {
+        handleCloseMovie();
+      }
+    });
+  }, []);
+
   useEffect(
     function () {
       const controller = new AbortController();
